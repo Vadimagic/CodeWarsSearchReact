24f923e690abe816396aa1db92313c49a61f1366
Vadim <vadim.prog.2000@gmail.com>
2020-11-27 23:55:17 +0300

    add img/icon

--- src/pages/Info/Info.js
+++ src/pages/Info/Info.js
@@ -11,11 +11,11 @@ export const Info = () => {
 			</p>
 			<div className="jumbotron__link-list">
 				<div className="jumbotron__link">
-					<img className="jumbotron__img" src="" alt="Codewars" />
+					<img className="jumbotron__img" src="./assets/img/codewars.png" alt="Codewars" />
 					<Link className="jumbotron__href" to="/users/Vadimagic">Мой Codewars</Link>
 				</div>
 				<div className="jumbotron__link">
-					<img className="jumbotron__img" src="" alt="Codewars" />
+					<img className="jumbotron__img" src="./assets/img/github.svg" alt="Codewars" />
 					<a className="jumbotron__href" href="https://www.codewars.com/users/Vadimagic" target="_blank" rel="noreferrer">Мой GitHub</a>
 				</div>
 			</div>
