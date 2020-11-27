130f6fb16f44fae26bb6f3d56eb5291f59df5486
Vadim <vadim.prog.2000@gmail.com>
2020-11-27 23:36:50 +0300

    add styles page info

---
+++ src/pages/Info/Info.js
@@ -0,0 +1,22 @@
+import './Info.scss'
+import { Link } from "react-router-dom"
+
+export const Info = () => {
+	return (
+		<div className="jumbotron">
+			<h1 className="jumbotron__title">CodewarSSearch</h1>
+			<p className="jumbotron__text">
+				SPA приложение, написанное на React + React Hooks.
+				Представляет из себя поиск информации с ресурса: Codewars (решение задач по программированию)
+			</p>
+				<div className="jumbotron__link">
+					<img className="jumbotron__img" src="" alt="Codewars" />
+					<Link className="jumbotron__href" to="/users/Vadimagic">Мой Codewars</Link>
+				</div>
+				<div className="jumbotron__link">
+					<img className="jumbotron__img" src="" alt="Codewars" />
+					<a className="jumbotron__href" href="https://www.codewars.com/users/Vadimagic" target="_blank" rel="noreferrer">Мой GitHub</a>
+				</div>
+		</div>
+	)
+}
