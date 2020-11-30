379b6f58c9d659b015f3c7866a9e4d610b4ab86c
Vadim <vadim.prog.2000@gmail.com>
2020-11-30 22:05:22 +0300

    create user card and style

--- src/context/codewars/codewarsState.js
+++ src/context/codewars/codewarsState.js
@@ -19,6 +19,7 @@ export const CodewarsState = ({children}) => {
 			{id : 2},
 			{id : 3},
 			{id : 4},
+			{id : 5},
 		]
 	}
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import { Search } from "../../components/Search/Search"
 import { CodewarsContext } from "../../context/codewars/codewarsContext"
+import './Home.scss'
 
 export const Home = () => {
 	const { users } = useContext(CodewarsContext)
@@ -16,7 +17,15 @@ export const Home = () => {
 				<div className="user-list">
 					{users.map( user => (
 						<div className="user" key={user.id}>
-							{user.id}
+							<div className="user__content">
+								<img src="https://html5css.ru/w3css/img_avatar3.png" alt="avatar" className="user__img"/>
+								<div className="user__info">
+									<div className="user__kata">3 kyu</div>
+									<div className="user__kata">exp: {user.id}</div>
+									<div className="user__kata">99</div>
+									<button className="user__btn">Open</button>
+								</div>
+							</div>
 						</div>
 					))}
 				</div>
